7a66dc2a281db9857094c0847a9734c651b048fa
EinsEli <57264430+EinsEli@users.noreply.github.com>
2024-06-17 13:28:27 +0200

    Update nav header

--- src/components/layout/nav-header.tsx
+++ src/components/layout/nav-header.tsx
@@ -6,6 +6,17 @@ import { navigationLinks } from "@/lib/navigation-Links";
 import { BarChartBig } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ModeToggle } from "./dark-mode-toggle";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 export default function NavHeader({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
@@ -18,21 +29,75 @@ export default function NavHeader({ children }: { children: React.ReactNode }) {
 						<Link href="#" className="flex items-center gap-2">
 							<BarChartBig className="h-6 w-6" />
 						</Link>
-						{navigationLinks.map((link) => {
-							return (
-								<Link
-									key={link.url}
-									href={link.url}
-									className={`text-muted-foreground transition-colors hover:text-foreground ${
-										link.url == pathname
-											? "!text-black font-semibold dark:!text-white"
-											: ""
-									}`}
-								>
-									{link.title}
-								</Link>
-							);
-						})}
+						<NavigationMenu>
+							<NavigationMenuList>
+								{navigationLinks.map((link) => {
+									if (link.url != null) {
+										return (
+											<NavigationMenuItem
+												key={link.url}
+												className="mr-4"
+											>
+												<Link
+													href="/overview"
+													className={`text-muted-foreground transition-colors hover:text-foreground ${
+														link.url == pathname
+															? "!text-black font-semibold dark:!text-white"
+															: ""
+													}`}
+												>
+													{link.title}
+												</Link>
+											</NavigationMenuItem>
+										);
+									} else if (link.subLinks != null) {
+										return (
+											<NavigationMenuItem
+												key={link.title}
+											>
+												<NavigationMenuTrigger>
+													{link.title}
+												</NavigationMenuTrigger>
+												<NavigationMenuContent>
+													<ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+														{link.subLinks.map(
+															(subLink) => (
+																<NavigationMenuItem
+																	key={
+																		subLink.title
+																	}
+																	title={
+																		subLink.title
+																	}
+																>
+																	<Link
+																		href={
+																			subLink.url
+																		}
+																		className="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+																	>
+																		<h1 className="text-md font-semibold">
+																			{
+																				subLink.title
+																			}
+																		</h1>
+																		<p className="text-sm text-muted-foreground">
+																			{
+																				subLink.description
+																			}
+																		</p>
+																	</Link>
+																</NavigationMenuItem>
+															)
+														)}
+													</ul>
+												</NavigationMenuContent>
+											</NavigationMenuItem>
+										);
+									}
+								})}
+							</NavigationMenuList>
+						</NavigationMenu>
 					</div>
 					<ModeToggle />
 				</nav>
--- src/lib/navigation-Links.ts
+++ src/lib/navigation-Links.ts
@@ -4,15 +4,28 @@ export const navigationLinks = [
 		url: '/overview',
 	},
 	{
-		title: 'Bubble Sort',
-		url: '/bubblesort',
+		title: "Linear Algorithms",
+		subLinks: [
+			{
+				title: 'Bubble Sort',
+				url: '/bubblesort',
+				description: 'Bubble Sort is a simple alorithm which swaps adjacent elements if they are in the wrong order.',
+			}
+		]
 	},
 	{
-		title: 'Merge Sort',
-		url: '/mergesort',
+		title: "Divide and Conquer Algorithms",
+		subLinks: [
+			{
+				title: 'Merge Sort',
+				url: '/mergesort',
+				description: 'Merge Sort divides the array into two halves, which are then merged later into a sorted array.',
+			},
+			{
+				title: 'Quick Sort',
+				url: '/quicksort',
+				description: 'Quick Sort picks a pivot element and partitions the given array around the picked pivot.',
+			}
+		]
 	},
-	{
-		title: 'Quick Sort',
-		url: '/quicksort',
-	}
 ];
