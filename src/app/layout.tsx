e4f5c55f446b7049c38c16f0bae79c2d676c258a
EinsEli <57264430+EinsEli@users.noreply.github.com>
2024-06-20 09:27:02 +0200

    Remove view transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ViewTransitions } from "next-view-transitions";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import "./globals.css";
 import { ContextProvider } from "@/components/layout/context";
@@ -18,18 +17,16 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ViewTransitions>
-			<html lang="en" suppressHydrationWarning>
-				<body className={inter.className}>
-					<ThemeProvider
-						attribute="class"
-						defaultTheme="system"
-						enableSystem
-					>
-						<ContextProvider>{children}</ContextProvider>
-					</ThemeProvider>
-				</body>
-			</html>
-		</ViewTransitions>
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+				>
+					<ContextProvider>{children}</ContextProvider>
+				</ThemeProvider>
+			</body>
+		</html>
 	);
 }
